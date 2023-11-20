64faee33039ec3c864576c9bdb3a57d1a08ed991
marcos <marcostulioav@gmail.com>
2023-11-20 16:28:40 -0300

    Primeiro commit

---
+++ HelloWorld1.java
@@ -0,0 +1,14 @@
+public class HelloWorld1 {
+
+    public static void main (String [] args){
+        String ambiente = args[0];
+        if (ambiente.equalsIgnoreCase("DEV")){
+            System.out.print("Executando em DEV");
+        } else if (ambiente.equalsIgnoreCase("TEST")){
+        System.out.print("Executando em TEST");
+        } else {
+            System.out.print(ambiente);
+        }
+
+    }
+}
